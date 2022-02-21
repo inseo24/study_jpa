5e14f821048b8ffa85ac1bdf392a432af44c6ea4
choi seoin <inseo@uupfi.com>
2022-02-21 14:10:17 +0900

    파일 변경

---
+++ ex1/src/main/kotlin/com/example/basic1/Basic1Application.kt
@@ -0,0 +1,11 @@
+package com.example.basic1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Basic1Application
+
+fun main(args: Array<String>) {
+	runApplication<Basic1Application>(*args)
+}
