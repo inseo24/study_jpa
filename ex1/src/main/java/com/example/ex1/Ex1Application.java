96d394d2045de18cfbc99f4a3a04dc3713f04185
choi seoin <inseo@uupfi.com>
2022-02-21 15:28:03 +0900

    add : limit, offset

--- ex1/src/main/java/com/example/ex1/Ex1Application.java
+++ ex1/src/main/java/com/example/ex1/Ex1Application.java
@@ -12,32 +12,34 @@ import java.util.List;
 @SpringBootApplication
 public class Ex1Application {
 
-	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
-		EntityManager em = emf.createEntityManager();
+        EntityManager em = emf.createEntityManager();
 
-		EntityTransaction tx = em.getTransaction();
-		tx.begin();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
 
-		try {
-			List<Member> result = em.createQuery("select m from Member as m", Member.class)
-							.getResultList();
+        try {
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(5)
+                    .getResultList();
 
-			for (Member member: result) {
-				System.out.println("member.name = " + member.getName());
-			}
-			tx.commit();
-		} catch (Exception e) {
-			tx.rollback();
-		} finally {
-			em.close();
-		}
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
 
-		emf.close();
+        emf.close();
 
 
-		SpringApplication.run(Ex1Application.class, args);
-	}
+        SpringApplication.run(Ex1Application.class, args);
+    }
 
 }
